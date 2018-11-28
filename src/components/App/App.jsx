e00e6a588f67a194037f7bcc335afbfa7fbf9b1d
phiathao <phiat11@gmail.com>
2018-11-28 14:24:05 -0600

    handle submit for input to save inside array

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,10 @@ import './App.css';
 
 class App extends Component {
   state = {
+    newMonster : {
+      name : '',
+      rate : 0
+    },
     monsterList: [
       {
         name: 'Mike Rozdowski',
@@ -18,6 +22,41 @@ class App extends Component {
         rate: 2
       }
     ]
+  }
+  handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log('handling submit');
+    let newMonster = this.state.newMonster;
+
+    this.setState({
+      monsterList: [
+        ...this.state.monsterList,
+        newMonster
+      ]
+    })
+
+    this.setState({ // reset input
+      newMonster : {
+        name: '',
+        rate: 0
+      }
+    })
+  }
+  handleNameChange = (event)=>{
+    this.setState({
+      newMonster : {
+        ...this.state.newMonster, // spread
+        name : event.target.value
+      }
+    })
+  }
+  handleRateChange = (event)=>{
+    this.setState({
+      newMonster : {
+        ...this.state.newMonster, // spread
+        rate : event.target.value
+      }
+    })
   }
   render() {
     // let listItemHtml = [];
@@ -36,10 +75,19 @@ class App extends Component {
     return (
       <div>
         <p>Lets get a list on the DOM</p>
+        <form onSubmit={this.handleSubmit}>
+        <input type='text' value={this.state.newMonster.name} placeholder='Monster Name' onChange={this.handleNameChange} />
+        <input type='number' value={this.state.newMonster.rate} placeholder='Scariness Rating' onChange={this.handleRateChange}/>
+        <input type="submit"/>
+        </form>
         {/* if there is only this.state.monsterList it will shove all of them together */}
         {/* {this.state.monsterList} */}
         {/* {JSON.stringify(this.state.monsterList)}  */}
-        {listItemHtml}
+        <pre>
+          <p>{this.state.newMonster.name} scariness rating is {this.state.newMonster.rate}</p>
+          {JSON.stringify(this.state.newMonster)}
+          {listItemHtml}
+        </pre>
       </div>
     );
   }
